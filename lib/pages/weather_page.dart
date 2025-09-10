bdcb4468e167927687ce8c18bed3259bf9ae1509
Kesaobaka Lekaote <lekaote45@gmail.com>
2025-09-10 20:17:13 +0200

    Pushing minimal code for now still busy with the rest

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WeatherPage extends StatelessWidget {
+  const WeatherPage({super.key});
+
+  
+    @override   
+    State<WeatherPage> createState() => _WeatherPageState();    
+
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+     
+    );
+  }
+}
---
+++ lib/services/weather_service.dart
@@ -0,0 +1,24 @@
+import '..models/weather.dart';
+import 'package:http/http.dart' as http;
+
+class WaetherService {
+ static const BASE_URL ="https://api.openweathermap.org/data/2.5/weather"; 
+    final String apiKey;
+
+   waetherSevice(this.apiKey);
+
+   Future<Weather> getWeather(String cityName async) {
+    final response = await http
+    .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+
+
+    if (response.statusCode == 200) {
+        final json = jsonDecode(response.body);
+        return Weather.fromJson(json);
+    } else {
+        throw Exception('Failed to load weather data');
+    }
+
+   }
+
+}
